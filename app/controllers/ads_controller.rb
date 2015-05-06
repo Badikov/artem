94699d03cc5a4d837d4adfe63a00386a9b6c772e
badikov-a@yandex.ru <badikov-a@yandex.ru>
2015-05-06 23:06:44 +0300

    first commit

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,14 +1,22 @@
 class AdsController < ApplicationController
+  respond_to :json
+
   def index
-    @ads = Ad.all
-    respond_to do |format|
-      format.json {render json: @ads}
-    end
+    respond_with Ad.all
+  end
+  def show
+    respond_with Ad.find(params[:id])
+  end
+
+  def update
+    respond_with Ad.update(params[:id], ad_params)
   end
   # POST /ads
   # POST /ads.json
   def create
-    @ad = Ad.new(ad_params)
+    respond_with Ad.create(ad_params)
+    #@ad = Ad.new(ad_params)
+=begin
     respond_to do |format|
       if @ad.save
         format.html { redirect_to @ad, notice: 'Ad was successfully created.' }
@@ -18,12 +26,17 @@ class AdsController < ApplicationController
         format.json { render json: @ad.errors, status: :unprocessable_entity }
       end
     end
+=end
+  end
+
+  def destroy
+    respond_with Ad.destroy(params[:id])
   end
 
   private
 
   def ad_params
-      params.require(:ad).permit(:phone, :price, 
-        :location, :marka_id, :model_id, :release_id, :gearbox_id, :steer_id, :state_id)
+      params.require(:ad).permit(:phone, :price, :location, :marka_id, 
+        :model_id, :release_id, :gearbox_id, :steer_id, :state_id)
     end
 end
